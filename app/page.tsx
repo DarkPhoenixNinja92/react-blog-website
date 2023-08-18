3d02dfd2672d6f7ecc730c6a9bd246f2236a871a
Shaun <scpour@gmail.com>
2023-08-18 13:03:07 -0400

    Added content to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,65 @@
 export default function Home() {
   return (
     <div>
-    <h1 className="absolute top-1/2 left-1/2 mt-10 text-6xl">Home</h1>
+    <h1 className="absolute top-1/6 left-1/3 mt-14 text-6xl">WrenchGnasher's Repair Shop</h1>
+    <p className="prev-txt absolute top-1/4 text-center w-4/6">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo
+    duis ut. Faucibus a pellentesque sit amet porttitor eget dolor. Imperdiet massa
+    tincidunt nunc pulvinar. Enim facilisis gravida neque convallis a. Tellus
+    pellentesque eu tincidunt tortor aliquam nulla facilisi cras. Morbi tempus
+    iaculis urna id volutpat. A arcu cursus vitae congue. Amet consectetur
+    adipiscing elit ut aliquam. Lacus vel facilisis volutpat est velit egestas dui.
+    Ut sem viverra aliquet eget sit. Semper risus in hendrerit gravida rutrum
+    quisque non tellus orci. Bibendum ut tristique et egestas quis. Tincidunt
+    praesent semper feugiat nibh sed pulvinar. Enim ut sem viverra aliquet eget.
+    Curabitur vitae nunc sed velit dignissim. Vel pharetra vel turpis nunc eget
+    lorem dolor sed. Duis ultricies lacus sed turpis tincidunt id aliquet risus.
+    Habitant morbi tristique senectus et netus. In ante metus dictum at tempor
+    commodo ullamcorper a lacus. Quis risus sed vulputate odio ut enim blandit
+    volutpat. Congue quisque egestas diam in arcu cursus euismod. Malesuada bibendum
+    arcu vitae elementum curabitur. Semper viverra nam libero justo. Tellus mauris a
+    diam maecenas sed enim. Nunc id cursus metus aliquam. Scelerisque varius morbi
+    enim nunc faucibus a pellentesque sit. In hac habitasse platea dictumst
+    vestibulum rhoncus est pellentesque elit. Mattis pellentesque id nibh tortor id
+    aliquet lectus. Convallis convallis tellus id interdum velit laoreet. Lectus
+    vestibulum mattis ullamcorper velit sed. Sed tempus urna et pharetra pharetra
+    massa massa ultricies mi. Sit amet commodo nulla facilisi nullam. Et malesuada
+    fames ac turpis egestas sed tempus urna. Risus viverra adipiscing at in tellus
+    integer feugiat scelerisque. In iaculis nunc sed augue lacus. Nulla facilisi
+    etiam dignissim diam. Erat nam at lectus urna duis convallis convallis tellus
+    id. Amet nisl purus in mollis nunc sed id semper. Eget velit aliquet sagittis id
+    consectetur purus ut faucibus pulvinar. Sapien et ligula ullamcorper malesuada
+    proin libero nunc. Egestas sed sed risus pretium quam vulputate dignissim. Orci
+    phasellus egestas tellus rutrum tellus pellentesque eu. Magna sit amet purus
+    gravida quis blandit. Ac turpis egestas maecenas pharetra convallis posuere
+    morbi. Dictum fusce ut placerat orci nulla pellentesque dignissim enim sit. Ac
+    orci phasellus egestas tellus rutrum tellus pellentesque eu tincidunt. Turpis
+    egestas pretium aenean pharetra magna ac placerat. Egestas integer eget aliquet
+    nibh praesent tristique. At quis risus sed vulputate odio ut enim blandit
+    volutpat. Fringilla est ullamcorper eget nulla facilisi etiam. Eu turpis egestas
+    pretium aenean pharetra magna ac placerat vestibulum. Sagittis nisl rhoncus
+    mattis rhoncus urna neque viverra. Ac turpis egestas integer eget aliquet. In
+    nisl nisi scelerisque eu ultrices vitae auctor. Morbi blandit cursus risus at
+    ultrices. Justo laoreet sit amet cursus sit amet dictum sit amet. Tortor at
+    risus viverra adipiscing. Turpis egestas maecenas pharetra convallis. Nec nam
+    aliquam sem et. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor
+    aliquam. Quisque egestas diam in arcu cursus euismod quis. Habitant morbi
+    tristique senectus et netus. Blandit libero volutpat sed cras ornare arcu dui.
+    Commodo quis imperdiet massa tincidunt. Commodo viverra maecenas accumsan lacus
+    vel facilisis volutpat est velit. Cras tincidunt lobortis feugiat vivamus at.
+    Dui vivamus arcu felis bibendum ut. Dignissim enim sit amet venenatis urna.
+    Massa id neque aliquam vestibulum. Sagittis nisl rhoncus mattis rhoncus. Et
+    tortor consequat id porta nibh venenatis cras. Sit amet justo donec enim diam
+    vulputate ut pharetra sit. Nec ultrices dui sapien eget mi proin. Volutpat ac
+    tincidunt vitae semper.
+</p>
+<div className="imagePreviews flex align-middle justify-center absolute left-1/4 top-2/3">
+  <img className="image-01 w-full h-36 md:h-48 object-cover mr-10" src="https://www.hirerush.com/blog/wp-content/uploads/2016/01/8620IMG00004.jpg" alt="" />
+  <img className="image-02 w-full h-36 md:h-48 object-cover mr-10" src="https://di-uploads-pod11.dealerinspire.com/shelormotormile/uploads/2020/05/Webp.net-resizeimage-29.jpg" alt="" />
+  <img className="image-03 w-full h-36 md:h-48 object-cover mr-10" src="https://i.pinimg.com/originals/f1/5d/c3/f15dc3761f35422ccd82541c8bf97d6f.jpg" alt="" />
+  <img className="image-04 w-full h-36 md:h-48 object-cover" src="https://vera-delightfull.com/wp-content/uploads/2019/02/Auto-Repair-Shop-1024x457.jpg" alt="" />
+</div>
     </div>
   )
 }
