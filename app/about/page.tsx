07875ee2a6d2d635d6371757237a91178da24848
Shaun <scpour@gmail.com>
2023-08-19 18:04:28 -0400

    Added basic contents to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,18 @@
 const About = () => {
     return (
-        <h1 className="absolute top-1/2 left-1/2 mt-10 text-6xl">About</h1>
+        <div className="about">
+        <h1 className="absolute top-1/6 left-1/2 mt-12 text-6xl">About Us</h1>
+<p className="about-para absolute top-1/4 left-1/3 text-center w-2/6">
+    Founded in Ontario, Canada in 1963, WrenchGnasher's Repair Shop migrated to New
+    York City in 1989 after a run of unsuccessful business decisons led to a near
+    bankruptcy. Today, WrenchGnashers is a multi-state company with locations across
+    32 states with major shops in New York, New Jersey, Virginia, Oregon and
+    California. They specialize in automotive repair but also have an active side
+    business in custom vehicle design including converting vans to mobile homes,
+    swapping out automatic transmissions for manuals and more.
+</p>
+<img className="image-preview" src="https://www.hirerush.com/blog/wp-content/uploads/2016/01/8620IMG00004.jpg" alt="" />
+        </div>
     )
 }
 
