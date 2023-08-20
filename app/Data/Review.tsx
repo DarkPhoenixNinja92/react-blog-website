bff91d5f055a43df5eb6647f1d8f3dc2fffb02ee
Shaun <scpour@gmail.com>
2023-08-20 19:05:31 -0400

    Touched up contact page and added data to the array of reviews to use on that page

---
+++ app/Data/Review.tsx
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 0,
+        reviewer: 'Joe Cobb',
+        stars: 4,
+        date: '09/21/1993',
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Non diam phasellus vestibulum lorem sed risus ultricies. Eu consequat ac felis donec."
+        +"Quis commodo odio aenean sed adipiscing diam. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Suscipit adipiscing bibendum est ultricies integer. Pretium quam vulputate dignissim suspendisse in est ante. Porttitor eget dolor morbi non."
+        +"Leo vel orci porta non. Nibh nisl condimentum id venenatis a. Cras tincidunt lobortis feugiat vivamus."
+        +"Diam volutpat commodo sed egestas egestas fringilla. Amet luctus venenatis lectus magna fringilla. Mi in nulla posuere sollicitudin aliquam ultrices. Est ante in nibh mauris cursus mattis molestie. Scelerisque viverra mauris in aliquam sem fringilla"
+        +"ut morbi tincidunt. Nisl pretium fusce id velit ut tortor pretium viverra. Venenatis a condimentum vitae sapien pellentesque habitant morbi. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Neque sodales ut etiam sit."
+        +"Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Feugiat nisl pretium fusce id. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dui accumsan sit amet nulla. Urna id volutpat lacus laoreet non curabitur gravida."
+    },
+    {
+        id: 1,
+        reviewer: 'Nail Hannah',
+        stars: 5,
+        date: '02/01/2013',
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Non diam phasellus vestibulum lorem sed risus ultricies. Eu consequat ac felis donec."
+        +"Quis commodo odio aenean sed adipiscing diam. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Suscipit adipiscing bibendum est ultricies integer. Pretium quam vulputate dignissim suspendisse in est ante. Porttitor eget dolor morbi non."
+        +"Leo vel orci porta non. Nibh nisl condimentum id venenatis a. Cras tincidunt lobortis feugiat vivamus."
+        +"Diam volutpat commodo sed egestas egestas fringilla. Amet luctus venenatis lectus magna fringilla. Mi in nulla posuere sollicitudin aliquam ultrices. Est ante in nibh mauris cursus mattis molestie. Scelerisque viverra mauris in aliquam sem fringilla"
+        +"ut morbi tincidunt. Nisl pretium fusce id velit ut tortor pretium viverra. Venenatis a condimentum vitae sapien pellentesque habitant morbi. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Neque sodales ut etiam sit."
+        +"Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Feugiat nisl pretium fusce id. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dui accumsan sit amet nulla. Urna id volutpat lacus laoreet non curabitur gravida."
+    },
+    {
+        id: 2,
+        reviewer: 'Dwight Ayesha',
+        stars: 1,
+        date: '11/21/2003',
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Non diam phasellus vestibulum lorem sed risus ultricies. Eu consequat ac felis donec."
+        +"Quis commodo odio aenean sed adipiscing diam. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Suscipit adipiscing bibendum est ultricies integer. Pretium quam vulputate dignissim suspendisse in est ante. Porttitor eget dolor morbi non."
+        +"Leo vel orci porta non. Nibh nisl condimentum id venenatis a. Cras tincidunt lobortis feugiat vivamus."
+        +"Diam volutpat commodo sed egestas egestas fringilla. Amet luctus venenatis lectus magna fringilla. Mi in nulla posuere sollicitudin aliquam ultrices. Est ante in nibh mauris cursus mattis molestie. Scelerisque viverra mauris in aliquam sem fringilla"
+        +"ut morbi tincidunt. Nisl pretium fusce id velit ut tortor pretium viverra. Venenatis a condimentum vitae sapien pellentesque habitant morbi. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Neque sodales ut etiam sit."
+        +"Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Feugiat nisl pretium fusce id. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dui accumsan sit amet nulla. Urna id volutpat lacus laoreet non curabitur gravida."
+    },
+    {
+        id: 3,
+        reviewer: 'Timo Svetlana',
+        stars: 4,
+        date: '12/31/2011',
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Non diam phasellus vestibulum lorem sed risus ultricies. Eu consequat ac felis donec."
+        +"Quis commodo odio aenean sed adipiscing diam. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Suscipit adipiscing bibendum est ultricies integer. Pretium quam vulputate dignissim suspendisse in est ante. Porttitor eget dolor morbi non."
+        +"Leo vel orci porta non. Nibh nisl condimentum id venenatis a. Cras tincidunt lobortis feugiat vivamus."
+        +"Diam volutpat commodo sed egestas egestas fringilla. Amet luctus venenatis lectus magna fringilla. Mi in nulla posuere sollicitudin aliquam ultrices. Est ante in nibh mauris cursus mattis molestie. Scelerisque viverra mauris in aliquam sem fringilla"
+        +"ut morbi tincidunt. Nisl pretium fusce id velit ut tortor pretium viverra. Venenatis a condimentum vitae sapien pellentesque habitant morbi. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Neque sodales ut etiam sit."
+        +"Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Feugiat nisl pretium fusce id. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dui accumsan sit amet nulla. Urna id volutpat lacus laoreet non curabitur gravida."
+    },
+    {
+        id: 4,
+        reviewer: 'Yosef Ramazan',
+        stars: 4,
+        date: '01/21/2000',
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Non diam phasellus vestibulum lorem sed risus ultricies. Eu consequat ac felis donec."
+        +"Quis commodo odio aenean sed adipiscing diam. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Suscipit adipiscing bibendum est ultricies integer. Pretium quam vulputate dignissim suspendisse in est ante. Porttitor eget dolor morbi non."
+        +"Leo vel orci porta non. Nibh nisl condimentum id venenatis a. Cras tincidunt lobortis feugiat vivamus."
+        +"Diam volutpat commodo sed egestas egestas fringilla. Amet luctus venenatis lectus magna fringilla. Mi in nulla posuere sollicitudin aliquam ultrices. Est ante in nibh mauris cursus mattis molestie. Scelerisque viverra mauris in aliquam sem fringilla"
+        +"ut morbi tincidunt. Nisl pretium fusce id velit ut tortor pretium viverra. Venenatis a condimentum vitae sapien pellentesque habitant morbi. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Neque sodales ut etiam sit."
+        +"Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Feugiat nisl pretium fusce id. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dui accumsan sit amet nulla. Urna id volutpat lacus laoreet non curabitur gravida."
+    },
+    {
+        id: 5,
+        reviewer: 'Berenice Ambre',
+        stars: 4,
+        date: '09/21/1993',
+        review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget duis at tellus at urna condimentum mattis. Non diam phasellus vestibulum lorem sed risus ultricies. Eu consequat ac felis donec."
+        +"Quis commodo odio aenean sed adipiscing diam. Felis bibendum ut tristique et egestas quis ipsum suspendisse ultrices. Suscipit adipiscing bibendum est ultricies integer. Pretium quam vulputate dignissim suspendisse in est ante. Porttitor eget dolor morbi non."
+        +"Leo vel orci porta non. Nibh nisl condimentum id venenatis a. Cras tincidunt lobortis feugiat vivamus."
+        +"Diam volutpat commodo sed egestas egestas fringilla. Amet luctus venenatis lectus magna fringilla. Mi in nulla posuere sollicitudin aliquam ultrices. Est ante in nibh mauris cursus mattis molestie. Scelerisque viverra mauris in aliquam sem fringilla"
+        +"ut morbi tincidunt. Nisl pretium fusce id velit ut tortor pretium viverra. Venenatis a condimentum vitae sapien pellentesque habitant morbi. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Neque sodales ut etiam sit."
+        +"Nisi quis eleifend quam adipiscing vitae proin sagittis nisl rhoncus. Feugiat nisl pretium fusce id. Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Dui accumsan sit amet nulla. Urna id volutpat lacus laoreet non curabitur gravida."
+    }
+]
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,18 @@
 const Contact = () => {
     return (
-        <h1 className="absolute top-1/2 left-1/2 mt-10 text-6xl">Contact</h1>
+        <div className="Contact">
+        <h1 className="absolute top-1/2 left-1/2 mt-10 text-6xl">Contact Us</h1>
+        <div className="contact-div">
+            <h3 className="main-office">Main Office</h3>
+            <div  className="address">
+            <p className="address-line-one">2589 West Main Street</p>
+            <p className="address-line-two">New York, NY 4589</p>
+            </div>
+            <p className="email">WrenchGnasherHR@gmail.com</p>
+            <p className="phone">(204) 194-9531</p>
+        </div>
+            <p className="note">Contact information is only provided for the main office. For contact details for one of thee branch offices, please contact the main office.</p>
+        </div>
     )
 }
 
